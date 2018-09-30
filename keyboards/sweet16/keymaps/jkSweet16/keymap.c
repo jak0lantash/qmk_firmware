770706bb2027e7f745ca41ed1a310ce17360383a
jak0lantash <git@jacquet.pro>
2018-09-30 11:08:13 +0100

    Added macros & boot indicator

--- keyboards/sweet16/keymaps/jkSweet16/keymap.c
+++ keyboards/sweet16/keymaps/jkSweet16/keymap.c
@@ -1,53 +1,142 @@
 #include "sweet16.h"
 
-enum custom_keycodes {
-  UP_URL = SAFE_RANGE
-};
-
 #define LAYER_MACROS 0
 #define LAYER_SETUP 1
+
+enum custom_keycodes {
+	M_CDSR = SAFE_RANGE,
+	M_SCOPY,
+	M_OUTLOOK,
+	M_MREMOTENG,
+	M_NOTEPAD,
+	M_VCENTER,
+	M_STE,
+	M_VMWARE,
+	M_HJVMWARE,
+	M_HJ,
+	M_JHGMAIL,
+	M_WAITCST,
+	M_REPLACE,
+	M_LOCK,
+	M_WMP,
+	M_TCPDUMP
+};
 
 #define _______ KC_TRNS
 #define xxxxxxx KC_NO
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[LAYER_MACROS] = LAYOUT_ortho_4x4(
-		KC_P7,		KC_P8,		KC_P9,		KC_PSLS,
-		KC_P4,		KC_P5,		KC_P6,		KC_PAST,
-		KC_P1,		KC_P2,		KC_P3,		KC_PMNS,
-		KC_P0,		LT(1, KC_LGUI), KC_PENT,	KC_PPLS),
+		MO(1),		M_TCPDUMP,	M_WMP,		M_REPLACE,
+		M_HJVMWARE,	M_HJ,		M_JHGMAIL,	M_WAITCST,
+		M_VCENTER,	M_STE,		M_VMWARE,	M_CDSR,
+		M_OUTLOOK,	M_MREMOTENG,	M_NOTEPAD,	M_SCOPY),
 
 	[LAYER_SETUP] = LAYOUT_ortho_4x4(
-		RGB_HUI,	RGB_SAI,	RGB_VAI,	RGB_M_R,
-		RGB_HUD,	RGB_SAD,	RGB_VAD,	RGB_M_SW,
-		RGB_TOG,	RGB_MOD,	RGB_RMOD,	RGB_M_P,
-		_______,	_______,	_______,	RESET)
+		_______,	_______,	_______,	RGB_M_R,
+		RGB_HUI,	RGB_SAI,	RGB_VAI,	RGB_M_SW,
+		RGB_HUD,	RGB_SAD,	RGB_VAD,	RGB_M_P,
+		RGB_TOG,	RGB_MOD,	RGB_RMOD,	RESET)
 };
 
 #define COLOR_BLUE_RGB 0x00, 0x00, 0xFF
 #define COLOR_BLUE_HSV 240, 255, 255
 #define COLOR_ORANGE_RGB 0xFF, 0x30, 0x00
-#define COLOR_ORANGE_HSV 37, 255, 200
+#define COLOR_ORANGE_HSV 25, 255, 255
 #define COLOR_CYAN_RGB 0x00, 0xF4, 0xFF
 #define COLOR_PINK_RGB 0xFF, 0x00, 0xFF
 #define COLOR_GREEN_RGB 0x40, 0xFF, 0x00
 #define COLOR_RED_RGB 0xFF, 0x00, 0x00
+#define COLOR_RED_HSV 0, 255, 255
 #define COLOR_YELLOW_RGB 0xFF, 0xFF, 0x00
+
+bool first_boot = true;
+int first_boot_time = 0;
 
 void matrix_init_user(void) {
 	//rgblight_init();
 	rgblight_enable();
-	rgblight_sethsv (COLOR_ORANGE_HSV);
+	rgblight_sethsv (COLOR_RED_HSV);
+	first_boot_time = timer_read();
+}
+
+void matrix_scan_user(void) {
+	if (first_boot && timer_elapsed(first_boot_time) > 1000) {
+		first_boot = false;
+		rgblight_sethsv (COLOR_ORANGE_HSV);
+	}
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case UP_URL:
-            if (record->event.pressed) {
-                SEND_STRING("http://1upkeyboards.com");
-            }
-            return false;
-            break;
+	if (record->event.pressed) switch (keycode) {
+		case M_OUTLOOK:
+			SEND_STRING(SS_DOWN(X_LGUI) SS_TAP(X_KP_1) SS_UP(X_LGUI));
+			return false;
+			break;
+		case M_MREMOTENG:
+			SEND_STRING(SS_DOWN(X_LGUI) SS_TAP(X_KP_2) SS_UP(X_LGUI));
+			return false;
+			break;
+		case M_NOTEPAD:
+			SEND_STRING(SS_DOWN(X_LGUI) SS_TAP(X_KP_3) SS_UP(X_LGUI));
+			return false;
+			break;
+		case M_SCOPY:
+			SEND_STRING(SS_DOWN(X_LCTRL) SS_DOWN(X_LALT) SS_TAP(X_C) SS_UP(X_LALT) SS_UP(X_LCTRL));
+			return false;
+			break;
+		case M_VCENTER:
+			//SEND_STRING("administrator@vsphere.local" SS_TAP(X_TAB) "VMware1!" SS_TAP(X_ENTER));
+			SEND_STRING(SS_DOWN(X_LCTRL) SS_TAP(X_F1) SS_UP(X_LCTRL));
+			return false;
+			break;
+		case M_STE:
+			//SEND_STRING("IAmOnThePhoneWithTechSupport" SS_TAP(X_ENTER));
+			SEND_STRING(SS_DOWN(X_LCTRL) SS_TAP(X_F2) SS_UP(X_LCTRL));
+			return false;
+			break;
+		case M_VMWARE:
+			//SEND_STRING("VMware1!");
+			SEND_STRING(SS_DOWN(X_LCTRL) SS_TAP(X_F3) SS_UP(X_LCTRL));
+			return false;
+			break;
+		case M_CDSR:
+			SEND_STRING(SS_DOWN(X_LCTRL) SS_DOWN(X_LALT) SS_TAP(X_D) SS_UP(X_LALT) SS_UP(X_LCTRL));
+			return false;
+			break;
+		case M_HJVMWARE:
+			//SEND_STRING("hjacquet@vmware.com");
+			SEND_STRING(SS_DOWN(X_LCTRL) SS_TAP(X_F6) SS_UP(X_LCTRL));
+			return false;
+			break;
+		case M_HJ:
+			//SEND_STRING("Hyacinthe Jacquet");
+			SEND_STRING(SS_DOWN(X_LCTRL) SS_TAP(X_F7) SS_UP(X_LCTRL));
+			return false;
+			break;
+		case M_JHGMAIL:
+			//SEND_STRING("jacquethyacinthe@gmail.com");
+			SEND_STRING(SS_DOWN(X_LCTRL) SS_TAP(X_F8) SS_UP(X_LCTRL));
+			return false;
+			break;
+		case M_WAITCST:
+			//SEND_STRING("Wait for cst feedback" SS_TAP(X_TAB) "Wait for cst");
+			SEND_STRING(SS_DOWN(X_LCTRL) SS_TAP(X_F5) SS_UP(X_LCTRL));
+			return false;
+			break;
+		case M_TCPDUMP:
+			//SEND_STRING("-o - | tcpdump-uw -enr - ");
+			SEND_STRING(SS_DOWN(X_LCTRL) SS_TAP(X_F4) SS_UP(X_LCTRL));
+			return false;
+			break;
+		case M_WMP:
+			SEND_STRING(SS_DOWN(X_LGUI) SS_TAP(X_KP_6) SS_UP(X_LGUI));
+			return false;
+			break;
+		case M_REPLACE:
+			SEND_STRING(SS_DOWN(X_LCTRL) SS_DOWN(X_LALT) SS_TAP(X_KP_0) SS_UP(X_LALT) SS_UP(X_LCTRL));
+			return false;
+			break;
     }
     return true;
 }
