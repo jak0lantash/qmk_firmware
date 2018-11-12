5a6cf78dc2f20d64cb679ecdb98fd4cf82b58f67
jak0lantash <git@jacquet.pro>
2018-11-12 01:42:40 +0000

    Added Fn layer

--- keyboards/dz60/keymaps/jk0/keymap.c
+++ keyboards/dz60/keymaps/jk0/keymap.c
@@ -1,14 +1,31 @@
 #include QMK_KEYBOARD_H
 
-#define FN KC_NO
+#ifndef xxxx
+#define xxxx KC_NO
+#endif
+#ifndef ____
+#define ____ KC_TRNS
+#endif
+#define LAYER_QWERTY 0
+#define LAYER_COLEMAK 1
+#define LAYER_FN 2
+#define FN MO(LAYER_FN) 
+#define LAYER RESET
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	LAYOUT_JK0(
-		KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, RESET,
+	[LAYER_QWERTY] = LAYOUT_JK0(
+		KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, LAYER,
 		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_ENT,
 		KC_BSPC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NUHS,
 		KC_LSFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_CAPS,
 		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, FN, KC_SPC, KC_RALT, KC_RGUI, KC_APP, KC_RCTL
+	),
+	[LAYER_FN] = LAYOUT_JK0(
+		KC_GRAVE, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_BSPC, RESET,
+		____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
+		____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
+		____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, KC_PGDN, KC_UP, KC_PGUP,
+		____, ____, ____, ____, ____, ____, ____, KC_LEFT, KC_DOWN, KC_RIGHT
 	)
 };
 
