39b9ed811e3f14983fad812d87ae55d9ea2f31d4
jak0lantash <git@jacquet.pro>
2018-11-12 01:20:27 +0000

    Improving dz60_jk0

--- keyboards/dz60/keymaps/jk0/keymap.c
+++ keyboards/dz60/keymaps/jk0/keymap.c
@@ -1,12 +1,14 @@
 #include QMK_KEYBOARD_H
 
+#define FN KC_NO
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  LAYOUT_60_iso_5x1u(
-    RESET,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   RESET,
-    RGB_M_R,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_ENT,
-    RGB_M_B,        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_BSLS,
-    RGB_M_SW,    KC_GRV,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,        KC_RSFT,
-    RGB_M_SN,     KC_LGUI,     KC_LALT,                      KC_SPC,                         KC_SPC, KC_LEFT, KC_DOWN,  KC_UP,  KC_RIGHT
-  )
+	LAYOUT_JK0(
+		KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, RESET,
+		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_ENT,
+		KC_BSPC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NUHS,
+		KC_LSFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_CAPS,
+		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, FN, KC_SPC, KC_RALT, KC_RGUI, KC_APP, KC_RCTL
+	)
 };
 
