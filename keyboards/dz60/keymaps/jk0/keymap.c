304cf2641c8f192d606f58f1ec304ae435b6e788
jak0lantash <git@jacquet.pro>
2018-12-23 14:15:36 +0000

    Added accented letters and tuned layers

--- keyboards/dz60/keymaps/jk0/keymap.c
+++ keyboards/dz60/keymaps/jk0/keymap.c
@@ -9,13 +9,14 @@
 
 #define LAYER_QWERTY 0
 #define LAYER_COLEMAK 1
-#define LAYER_FN 2
-#define LAYER_LAYERS 3
-#define LAYER_TESTER 4
-#define LAYER_TESTER_BACK 5
+#define LAYER_ACCENTS 2
+#define LAYER_FN 3
+#define LAYER_LAYERS 4
+#define LAYER_TESTER 5
+#define LAYER_TESTER_BACK 6
 
 #define FN MO(LAYER_FN) 
-#define LAYER MO(LAYER_LAYERS)
+#define LAYER LT(LAYER_LAYERS, KC_BSPC)
 #define TESTER TG(LAYER_TESTER)
 #define TST_BK MO(LAYER_TESTER_BACK)
 #define QWERTY TO(LAYER_QWERTY)
@@ -26,7 +27,25 @@
 
 enum custom_keycodes {
 	M_GRAVE = SAFE_RANGE,
-	M_GRAVE3
+	M_GRAVE3,
+	M_ACIRC,
+	M_AGRAV,
+	M_AUML,
+	M_EGRAV,
+	M_EACCU,
+	M_EUML,
+	M_ECIRC,
+	M_AELIG,
+	M_CCEDL,
+	M_YUML,
+	M_UCIRC,
+	M_UGRAV,
+	M_UUML,
+	M_ICIRC,
+	M_IUML,
+	M_OCIRC,
+	M_OUML,
+	M_OELIG
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -35,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_ENT,
 		KC_BSPC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NUHS,
 		KC_LSFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_UP), KC_CAPS,
-		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, FN, KC_ENT, KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT
+		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, FN, LT(LAYER_ACCENTS, KC_ENT), KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT
 	),
 	[LAYER_COLEMAK] = LAYOUT_JK0(
 		KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, LAYER,
@@ -44,8 +63,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LSFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_CAPS,
 		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, FN, LSFT_T(KC_ENT), KC_RALT, KC_RGUI, KC_APP, KC_RCTL
 	),
+	[LAYER_ACCENTS] = LAYOUT_JK0(
+		M_GRAVE, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
+		M_GRAVE3, M_ACIRC, M_EACCU, M_EGRAV, ____, M_YUML, M_UCIRC, M_ICIRC, M_OCIRC, M_OELIG, ____, ____, ____, ____,
+		____, M_AGRAV, M_AUML, M_EUML, ____, ____, M_UGRAV, M_UUML, M_IUML, M_OUML, ____, ____, ____,
+		____, ____, ____, M_AELIG, M_CCEDL, ____, ____, ____, ____, ____, ____, ____, ____, ____,
+		____, ____, ____, ____, ____, ____, ____, ____, ____, ____
+	),
 	[LAYER_FN] = LAYOUT_JK0(
-		M_GRAVE, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, RESET,
+		M_GRAVE, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, xxxx,
 		M_GRAVE3, KC_MUTE, KC_VOLD, KC_VOLU, ____, ____, ____, ____, ____, ____, ____, ____, KC_INSERT, ____,
 		____, KC_MPLY, KC_MPRV, KC_MNXT, ____, ____, ____, ____, ____, ____, KC_PSCREEN, KC_SCROLLLOCK, KC_PAUSE,
 		____, ____, ____, ____, ____, ____, ____, ____, ____, KC_APP, KC_PGUP, KC_HOME, KC_UP, KC_END,
@@ -56,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		xxxx, BL_TOGG, BL_STEP, BL_BRTG, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx,
 		xxxx, RGB_TOG, RGB_MOD, RGB_RMOD, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_G, RGB_M_X, xxxx,
 		xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx,
-		xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx
+		RESET, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx
 	),
 	[LAYER_TESTER] = LAYOUT_JK0(
 		xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, xxxx, TST_BK,
@@ -105,11 +131,14 @@ uint32_t layer_state_set_user(uint32_t state) {
 			rgblight_setrgb (COLOR_GREEN_RGB);
 			break;
 		case LAYER_LAYERS:
-			rgblight_setrgb (COLOR_YELLOW_RGB);
+			rgblight_setrgb (COLOR_RED_RGB);
 			break;
 		case LAYER_TESTER:
 		case LAYER_TESTER_BACK:
 			rgblight_setrgb (COLOR_TURQUOISE_RGB);
+			break;
+		case LAYER_ACCENTS:
+			rgblight_setrgb (COLOR_PINK_RGB);
 			break;
 	}
 	return state;
@@ -127,6 +156,78 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 				SS_DOWN(X_LALT) SS_TAP(X_KP_9) SS_TAP(X_KP_6) SS_UP(X_LALT)
 				SS_DOWN(X_LALT) SS_TAP(X_KP_9) SS_TAP(X_KP_6) SS_UP(X_LALT)
 			);
+			return false;
+			break;
+		case M_AGRAV: 
+			SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_2) SS_TAP(X_KP_4) SS_UP(X_LALT));
+			return false;
+			break;
+		case M_ACIRC: 
+			SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_2) SS_TAP(X_KP_6) SS_UP(X_LALT));
+			return false;
+			break;
+		case M_AUML: 
+			SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_2) SS_TAP(X_KP_8) SS_UP(X_LALT));
+			return false;
+			break;
+		case M_EGRAV: 
+			SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_3) SS_TAP(X_KP_2) SS_UP(X_LALT));
+			return false;
+			break;
+		case M_EACCU: 
+			SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_3) SS_TAP(X_KP_3) SS_UP(X_LALT));
+			return false;
+			break;
+		case M_ECIRC: 
+			SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_3) SS_TAP(X_KP_4) SS_UP(X_LALT));
+			return false;
+			break;
+		case M_EUML: 
+			SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_3) SS_TAP(X_KP_5) SS_UP(X_LALT));
+			return false;
+			break;
+		case M_ICIRC: 
+			SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_3) SS_TAP(X_KP_8) SS_UP(X_LALT));
+			return false;
+			break;
+		case M_IUML: 
+			SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_3) SS_TAP(X_KP_9) SS_UP(X_LALT));
+			return false;
+			break;
+		case M_OCIRC: 
+			SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_4) SS_TAP(X_KP_4) SS_UP(X_LALT));
+			return false;
+			break;
+		case M_OUML: 
+			SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_4) SS_TAP(X_KP_6) SS_UP(X_LALT));
+			return false;
+			break;
+		case M_UGRAV: 
+			SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_4) SS_TAP(X_KP_9) SS_UP(X_LALT));
+			return false;
+			break;
+		case M_UCIRC: 
+			SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_5) SS_TAP(X_KP_1) SS_UP(X_LALT));
+			return false;
+			break;
+		case M_UUML: 
+			SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_5) SS_TAP(X_KP_2) SS_UP(X_LALT));
+			return false;
+			break;
+		case M_YUML: 
+			SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_5) SS_TAP(X_KP_5) SS_UP(X_LALT));
+			return false;
+			break;
+		case M_CCEDL: 
+			SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_3) SS_TAP(X_KP_1) SS_UP(X_LALT));
+			return false;
+			break;
+		case M_AELIG: 
+			SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_3) SS_TAP(X_KP_0) SS_UP(X_LALT));
+			return false;
+			break;
+		case M_OELIG: 
+			SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_KP_0) SS_TAP(X_KP_1) SS_TAP(X_KP_5) SS_TAP(X_KP_6) SS_UP(X_LALT));
 			return false;
 			break;
 	}
